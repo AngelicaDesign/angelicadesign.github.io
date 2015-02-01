3e23df7d1f6d008971a6a22f79c0aac0da33b13b
Mark Miro <markmirony+github@gmail.com>
2015-02-01 12:00:13 -0800

    Got basic about page done. Refactored a lot of css

--- gulpfile.js
+++ gulpfile.js
@@ -84,11 +84,8 @@ gulp.task('fonts', function () {
 // Compile and Automatically Prefix Stylesheets
 gulp.task('styles', function () {
   // For best performance, don't add Sass partials to `gulp.src`
-  return gulp.src([
-    'app/styles/*.scss',
-    'app/styles/**/*.css'
-  ])
-    .pipe($.changed('.tmp/styles', {extension: '.css'}))
+  return gulp.src('app/styles/main.scss')
+    // .pipe($.changed('.tmp/styles', {extension: '.css'}))
     .pipe($.sass({
       precision: 10,
       onError: console.error.bind(console, 'Sass error:'),
