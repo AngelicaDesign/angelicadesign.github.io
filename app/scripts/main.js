f1b536f82b1413e67d4b55ac385217211b1bffe1
Mark Miro <markmirony+github@gmail.com>
2015-02-02 02:05:57 -0800

    Added scroll to top.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,15 +3,22 @@
 (function () {
   'use strict';
 
-  $(document).ready(function(){
+  $(document).ready(function () {
     if ($('.home-page').length) {
       console.log($('.nav'));
-      window.addEventListener('scroll', function (e) {
+      window.addEventListener('scroll', function () {
         $('.nav').toggle(window.scrollY > $('.header').outerHeight() - $('.nav').outerHeight());
+        $('.to-top-button').toggle(window.scrollY > window.innerHeight);
       });
     } else {
       $('.nav').sticky();
     }
+
+    $('a.to-top-button').click(function () {
+      $('html, body').animate({ scrollTop: 0 }, 'slow');
+      return false;
+    });
+
   });
 
 })();
