6ffa988ec4ea379d11c63410fa472515cf37a7c9
Mark Miro <markmirony+github@gmail.com>
2015-02-02 12:58:05 -0800

    Made contact page work. Added class to each nav link

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,8 +4,10 @@
   'use strict';
 
   $(document).ready(function () {
+
+    // Enable sticky nav on all except home page because on that
+    // page we have it disappear until user scroll down.
     if ($('.home-page').length) {
-      console.log($('.nav'));
       window.addEventListener('scroll', function () {
         $('.nav').toggle(window.scrollY > $('.header').outerHeight() - $('.nav').outerHeight());
         $('.to-top-button').toggle(window.scrollY > window.innerHeight);
@@ -14,8 +16,18 @@
       $('.nav').sticky();
     }
 
+    // Scroll to top when thi button is clicked
     $('a.to-top-button').click(function () {
       $('html, body').animate({ scrollTop: 0 }, 'slow');
+      return false;
+    });
+
+    // Make contact link in nav trigger hiding and showing the contact page
+    $('a.contact-link').attr('href', '#');
+    $('a.contact-link').click(function () {
+      $('.contact-page').toggleClass('hide');
+      $('body').toggleClass('no-scroll');
+      $('.nav').toggleClass('force-show');
       return false;
     });
 
