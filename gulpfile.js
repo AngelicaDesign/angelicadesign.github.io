a56cd717ec0b18c066871f9b929f1140cae232c0
Mark Miro <markmirony+github@gmail.com>
2015-02-16 14:19:36 -0800

    Added integration for gulp-devtools

--- gulpfile.js
+++ gulpfile.js
@@ -199,3 +199,5 @@ gulp.task('deploy', ['default'], function () {
 
 // Load custom tasks from the `tasks` directory
 // try { require('require-dir')('tasks'); } catch (err) { console.error(err); }
+
+module.exports = gulp;
